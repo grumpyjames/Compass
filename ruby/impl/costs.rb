228934760a8c8aa96a536512aad914e9a6769489
James Byatt <grumpyjames@gmail.com>
2009-07-04 17:15:10 +0100

    Remove debug messages from player test, add road and ship as resources also

--- ruby/impl/costs.rb
+++ ruby/impl/costs.rb
@@ -9,7 +9,7 @@ class Costs
     #place any negative numbers towards the beginning - the test code that deals with this isn't robust enough to cope otherwise
     BuildingType::CITY => {  Resource::GRAIN => 2, Resource::SETTLEMENT => -1, Resource::ORE => 3, Resource::CITY => 1},
     DEV_CARD => { Resource::WOOL => 1, Resource::ORE => 1, Resource::GRAIN => 1 },
-    EdgeBuildingType::ROAD => {Resource::BRICK => 1, Resource::LUMBER => 1},
-    EdgeBuildingType::SHIP => {Resource::WOOL => 1, Resource::LUMBER => 1}
+    EdgeBuildingType::ROAD => {Resource::BRICK => 1, Resource::LUMBER => 1, Resource::ROAD => 1},
+    EdgeBuildingType::SHIP => {Resource::WOOL => 1, Resource::LUMBER => 1, Resource::SHIP => 1}
   }   
 end
